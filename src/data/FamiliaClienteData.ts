cf02b75ff8801bf4025d1146fbeb9d9b7cdc52ec
Henriquesantos12 <114838533+Henriquesantos12@users.noreply.github.com>
2023-06-14 18:01:57 -0300

    70% do data feito

--- src/data/FamiliaClienteData.ts
+++ src/data/FamiliaClienteData.ts
@@ -1,17 +1,43 @@
-// import { CustomError } from "../error/CustomError";
-// import { BaseData } from "./BaseData";
+import { CustomError } from "../error/CustomError";
+import { BaseData } from "./BaseData";
+import { FamiliaClienteModel } from "../model/FamiliaClienteModel";
 
-// export class FamiliaClienteData extends BaseData{
-//   protected tableName:string = "familia_cliente"
-//   public async createFamiliaCliente(familiaCliente: ): Promise<void>{
-//     try {
-//       await BaseData.connection(this.tableName).insert({
-        
-//       })
-      
-//     }catch (error:any) {
-//       throw new CustomError(400, error.sqlMessage);
+export class FamiliaClienteData extends BaseData{
+  protected tableName:string = "familia_cliente"
+  public async createFamiliaCliente(familiaCliente: FamiliaClienteModel): Promise<void>{
+    try {
+      await BaseData.connection(this.tableName).insert({
+        id_familia_cliente: familiaCliente.getId_familia_cliente(),
+        id_cliente: familiaCliente.getId_cliente(),
+        id_familia: familiaCliente.getId_familia()
+      })
+    }  catch (error:any) {
+       throw new CustomError(400, error.sqlMessage);
+     }
+  }
+  public async findFamiliaClientebyID(
+    id_familia_cliente: string
+  ): Promise<FamiliaClienteModel | undefined> {
+      try {
+          const [familiaCliente]: FamiliaClienteModel[] = await BaseData.connection(
+            this.tableName
+          )
+              .select("*")
+              .where ({ id_familia_cliente: id_familia_cliente });
+            return familiaCliente;
+         } catch (error: any) {
+            throw new CustomError(400,error.sqlMessage);
+         }
+  }
 
-//   }
-//   }
-// }
+  public async getFamiliaCliente() {
+    try {
+    const results = await FamiliaClienteData.connection(this.tableName).select(
+        "*"
+    );
+    return results;
+    } catch (error: any) {
+        throw new CustomError(400,error.sqlMessage);
+    }
+  }
+}
--- src/data/FormularioData.ts
+++ src/data/FormularioData.ts
@@ -1,17 +1,52 @@
-// import { CustomError } from "../error/CustomError";
-// import { BaseData } from "./BaseData";
+import { CustomError } from "../error/CustomError";
+import { BaseData } from "./BaseData";
+import { FormularioModel } from "../model/FormularioModel";
 
-// export class FormularioData extends BaseData{
-//   protected tableName:string = "formulario"
-//   public async createFormulario(formulario: ): Promise<void>{
-//     try {
-//       await BaseData.connection(this.tableName).insert({
-        
-//       })
+export class FormularioData extends BaseData{
+  protected tableName:string = "formulario";
+  
+public async createFormulario(forms:  FormularioModel): Promise<void> {
+    try {
+      await BaseData.connection(this.tableName).insert({
+        id_formulario: forms.getId_formulaio(),
+        nome: forms.getNome(),
+        objetivo: forms.getObjetivo(),
+        altura: forms.getAltura(),
+        idade: forms.getIdade(),
+        peso: forms.getPeso(),
+        capacidade_fisica: forms.getCapacidade_fisica(),
+        restricao_alimentar: forms.getRestricao_alimentar(),
+        tempo_preparo: forms.getTempo_preparo(),
+        foto: forms.getFoto(),
+        id_cliente: forms.getId_cliente()
+      })
       
-//     }catch (error:any) {
-//       throw new CustomError(400, error.sqlMessage);
-
-//   }
-//   }
-// }
+    }  catch (error:any) {
+         throw new CustomError(400, error.sqlMessage);
+    }
+}
+public async findFormularioById(
+    id_formulario: string
+): Promise<FormularioModel | undefined> {
+    try {
+        const [forms]: FormularioModel[] = await BaseData.connection(
+            this.tableName
+        )
+            .select("*")
+            .where({id_formulario: id_formulario });
+            return forms;
+    } catch (error: any) {
+        throw new CustomError(400,error.sqlMessage);
+    }
+}
+public async getPlanos() {
+    try {
+        const results = await FormularioData.connection(this.tableName).select(
+            "*"
+        );
+return results;
+    } catch (error: any) {
+    throw new CustomError(400, error.sqlMessage);
+    } 
+}
+}
